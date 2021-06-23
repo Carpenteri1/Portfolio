06b9b0dffc88ea6dbd62aa2ffe9b29188cca09b0
Carpenteri1 <niclastimle@protonmail.com>
2021-06-23 20:05:41 +0200

    about page done and small changes to start and other pages

--- src/app/pagecomponents/aboutpage/aboutpage.component.ts
+++ src/app/pagecomponents/aboutpage/aboutpage.component.ts
@@ -5,5 +5,5 @@ import { Component } from '@angular/core';
   styleUrls: ['../../../../node_modules/bootstrap/dist/css/bootstrap.min.css']
 })
 export class AboutPageComponent {
-  public pdfSrc = "../../../assets/files/CV-NiclasTimle.pdf";
+  //public pdfSrc = "../../../assets/files/CV-NiclasTimle.pdf";
 }

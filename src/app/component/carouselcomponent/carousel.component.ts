6efc1e07b5a2e2efecf99f7be534837e0d72ac60
Carpenteri1 <niclastimle@protonmail.com>
2021-06-23 11:04:43 +0200

    updated docs

--- src/app/component/carouselcomponent/carousel.component.ts
+++ src/app/component/carouselcomponent/carousel.component.ts
@@ -8,31 +8,47 @@ import { Component, OnInit } from "@angular/core";
 
 export class CarouselComponent implements OnInit{
     
-    titleContent = [".Net Developer","Frontend / Backend Developer","Angular Developer"];
-    textContet = ["Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    "Nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit",
-    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]
+   
     currentTitleMessage:string = "";
     currentTextMessage:string ="";
-    carouselTime(){
-        this.currentTitleMessage = this.titleContent[0];
-        this.currentTextMessage = this.textContet[0];
-        for(let i = 1;i <= this.titleContent.length;i++)
+    carouselTime(index:any){
+        let titleContent = ["",".Net Developer","Frontend Developer","Backend Developer","Angular Developer",""];
+        let textContet = ["","Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        "Nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit",
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",""]
+        this.currentTitleMessage = titleContent[0];
+
+        for(let i = index;i <= titleContent.length;)
         {
-            setTimeout(() => {
-                this.currentTitleMessage = this.titleContent[i];
-                this.currentTextMessage = this.textContet[i];
-                if(i ===  this.titleContent.length){
-                    this.carouselTime();
-                }
-            }, 1500*(i+1));
-            
+                setTimeout(() => {
+                    console.log('else');
+                    if(i<=titleContent.length){
+                        this.currentTitleMessage = titleContent[i];
+                        this.currentTextMessage = textContet[i];
+                        i++
+                    }
+                    else
+                    {
+                        console.log('else');
+                        this.carouselTime(1);
+                    }
+                },4000*(i+1));       
         }
+        
+    }
+
+    callMethod(i:number) : any{
+       if(i < 5){
+            return 4000*(i+1);
+       }
+       if(i > 5){
+            return 1000*(i+1);
+       }  
     }
 
     ngOnInit() 
-    {
-        this.carouselTime();
+    { 
+        this.carouselTime(1);
     }
 }

9d3484ce2288aaf5543d718da76070426c4a3e0e
Niclas Timle <niclas.timle@optidev.se>
2022-04-03 18:52:10 +0200

    clean code

--- src/app/pagecomponents/startpage/startpage.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'startpage-component',
-  templateUrl: 'startpage.component.html',
-  styleUrls: ['startpage.component.css','../../../../node_modules/bootstrap/dist/css/bootstrap.min.css']
-})
-export class StartPageComponent {
-
-}

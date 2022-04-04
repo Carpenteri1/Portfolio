90379de20eb78c33e07e5e516c6aac4fbff1bdeb
Niclas Timle <niclas.timle@optidev.se>
2022-04-04 23:21:08 +0200

    fixed github pages

--- src/app/component/footercomponent/footer.component.ts
+++ src/app/component/footercomponent/footer.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector:'footer-component',
     templateUrl:'footer.component.html',
-    styleUrls:['../../../../node_modules/bootstrap/dist/css/bootstrap.min.css','footer.component.css']
+    styleUrls:['../../../../node_modules/bootstrap/dist/css/Bootstrap.min.css','footer.component.css']
 })
 
 export class FooterComponent{

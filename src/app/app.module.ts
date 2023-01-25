31c9db92913bd587d448b3c52982cd2388adea6f
Carpenteri1 <niclastimle@icloud.com>
2023-01-25 01:40:57 +0100

    remove broken pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,10 @@ import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 import { HeaderComponent } from './component/headercomponent/header.component';
 import { FooterComponent } from './component/footercomponent/footer.component';
-import { PortfolioComponent } from './pagecomponents/portfoliopage/portfoliopage.component';
 import { StartPageComponent} from './pagecomponents/startpage/startpage.component'
-import { AboutPageComponent } from './pagecomponents/aboutpage/aboutpage.component';
 import { CarouselComponent } from './component/carouselcomponent/carousel.component';
-import { ReferenserPageComponent } from './pagecomponents/referenserpage/referenserpage.component';
-import { TestPageComponent } from './pagecomponents/testpage/testpage.component';
 
 @NgModule({
   declarations: [
@@ -23,11 +18,7 @@ import { TestPageComponent } from './pagecomponents/testpage/testpage.component'
     HeaderComponent,
     FooterComponent,
     StartPageComponent,
-    AboutPageComponent,
-    PortfolioComponent,
-    CarouselComponent,
-    ReferenserPageComponent,
-    TestPageComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,  

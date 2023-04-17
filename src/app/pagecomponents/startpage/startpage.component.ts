bb4c5c154f671b5b297a9210332e704ddfd5d8e5
Carpenteri1 <niclastimle@icloud.com>
2023-04-18 00:48:26 +0200

    B1 Make dots to right of page to navigate between page components.

--- src/app/pagecomponents/startpage/startpage.component.ts
+++ src/app/pagecomponents/startpage/startpage.component.ts
@@ -1,10 +1,40 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {ViewEncapsulation , ElementRef, ViewChild, AfterViewInit, Component} from '@angular/core';
+
 @Component({
   selector: 'startpage-component',
   encapsulation: ViewEncapsulation.None,
   templateUrl: 'startpage.component.html',
   styleUrls: ['startpage.component.css']
 })
-export class StartPageComponent {
+export class StartPageComponent implements AfterViewInit{
+  @ViewChild('scrollDotsContainer') scrollDotsContainer!: ElementRef;
+  activeIndex: number = 0;
 
+  sections = [
+    { id: 'section1', label: 'Section 1' },
+    { id: 'section2', label: 'Section 2' },
+    { id: 'section3', label: 'Section 3' }
+  ];
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.createDots();
+  }
+
+  createDots() {
+  for (let i = 0; i < this.sections.length; i++) {
+    const dot = document.createElement('span');
+    dot.classList.add('scroll-dot');
+    dot.addEventListener('click', () => this.scrollTo(i.toString()));
+    this.scrollDotsContainer.nativeElement.appendChild(dot);
+    };
+  }
+
+  scrollTo(id: string) {
+    const element = document.getElementById(id);    
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }

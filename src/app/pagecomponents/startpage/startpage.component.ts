a413675b6bf08b87b41c8b2536e3c30e15330ed1
Carpenteri1 <niclastimle@icloud.com>
2023-04-18 21:59:12 +0200

    removed scrollbar and added dots for navigation

--- src/app/pagecomponents/startpage/startpage.component.ts
+++ src/app/pagecomponents/startpage/startpage.component.ts
@@ -1,4 +1,4 @@
-import {ViewEncapsulation , ElementRef, ViewChild, AfterViewInit, Component} from '@angular/core';
+import {ViewEncapsulation, Component} from '@angular/core';
 
 @Component({
   selector: 'startpage-component',
@@ -6,35 +6,8 @@ import {ViewEncapsulation , ElementRef, ViewChild, AfterViewInit, Component} fro
   templateUrl: 'startpage.component.html',
   styleUrls: ['startpage.component.css']
 })
-export class StartPageComponent implements AfterViewInit{
-  @ViewChild('scrollDotsContainer') scrollDotsContainer!: ElementRef;
-  activeIndex: number = 0;
-
-  sections = [
-    { id: 'section1', label: 'Section 1' },
-    { id: 'section2', label: 'Section 2' },
-    { id: 'section3', label: 'Section 3' }
-  ];
-
-  ngOnInit() {}
-
-  ngAfterViewInit() {
-    this.createDots();
-  }
-
-  createDots() {
-  for (let i = 0; i < this.sections.length; i++) {
-    const dot = document.createElement('span');
-    dot.classList.add('scroll-dot');
-    dot.addEventListener('click', () => this.scrollTo(i.toString()));
-    this.scrollDotsContainer.nativeElement.appendChild(dot);
-    };
-  }
-
-  scrollTo(id: string) {
-    const element = document.getElementById(id);    
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
+export class StartPageComponent   {
+  scroll(el: HTMLElement) {
+      el.scrollIntoView({ behavior: 'smooth' });
   }
 }

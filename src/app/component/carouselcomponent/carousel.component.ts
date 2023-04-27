527b145730bf54e1ec0419fb7bec481f147bc33e
Carpenteri1 <niclastimle@icloud.com>
2023-04-28 00:36:54 +0200

    fix progressbar, more color changes

--- src/app/component/carouselcomponent/carousel.component.ts
+++ src/app/component/carouselcomponent/carousel.component.ts
@@ -11,22 +11,22 @@ import { Component, OnInit } from "@angular/core";
 export class CarouselComponent implements OnInit {
 
     skillList1: Skill[] = [
-        { name: "C#", level: 100 },
         { name: ".Net", level: 80 },
         { name: "Asp.Net", level: 80 },
         { name: "Xamarin", level: 60 },
         { name: "Entity Framework", level: 50 },
         { name: "Dapper", level: 60 },
         { name: "SQL/T-SQL", level: 60 },
-        { name: "Docker", level: 30 },
+        { name: "Docker", level: 45 },
         { name: "Kubernetes", level: 10 },
     ];
 
     skillList2: Skill[] = [
         { name: "Html", level: 80 },
         { name: "CSS/Boostrap", level: 60 },
+        { name: "C#", level: 100 },
         { name: "Javascript", level: 50 },
-        { name: "Angular / Typescript", level: 60 },
+        { name: "Typescript", level: 40 },
         { name: "XML", level: 75 },
         { name: "JSON", level: 80 },
         { name: "Yalm", level: 35 },
@@ -34,22 +34,22 @@ export class CarouselComponent implements OnInit {
     ];
 
     skillList3: Skill[] = [
-        { name: "npm", level: 70 },
+        { name: "npm", level: 50 },
         { name: "Git", level: 80 },
+        { name: "Angular", level: 60 },
     ];
 
     listOfSkills: Skill[][] = [
         this.skillList1,
-        this.skillList2
+        this.skillList2,
+        this.skillList3
     ];
 
     index: number = 0;
-    currentListOfSKills: Skill[] = [];
-    fadeIn: boolean = false;
-    fadeOut: boolean = false;
+    currentListOfSKills: Skill[] = this.listOfSkills[this.index];
+    
     carouselTime() {
-        //setInterval(() => this.setMessage(), 5000);
-        this.currentListOfSKills = this.listOfSkills[0];
+        setInterval(() => this.setMessage(), 5000);
     }
     setMessage() {
         if (this.index <= 2) {

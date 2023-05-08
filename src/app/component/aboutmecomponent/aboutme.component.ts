6ccd2e8b3802f331875524ecbecebd04bcd06756
Carpenteri1 <niclastimle@icloud.com>
2023-05-08 22:17:33 +0200

    more about, create component

---
+++ src/app/component/aboutmecomponent/aboutme.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'aboutme-component',
+    templateUrl: 'aboutme.component.html',
+    styleUrls: ['aboutme.component.css'],
+})
+
+export class AboutMeComponent implements OnInit {
+    ngOnInit() {
+    }
+}

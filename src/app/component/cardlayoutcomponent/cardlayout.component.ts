4d66a1b8a525ac7c62326d556e60c54b55403552
Carpenteri1 <niclastimle@icloud.com>
2023-05-08 22:59:22 +0200

    Add list of repos github

---
+++ src/app/component/cardlayoutcomponent/cardlayout.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'cardlayout-component',
+    templateUrl: 'cardlayout.component.html'
+})
+
+export class CardLayoutComponent implements OnInit {
+    ngOnInit() {
+    }
+}

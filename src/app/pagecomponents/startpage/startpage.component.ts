e40770f23e113b8031c60ab51c648a7212ec2fa5
Carpenteri1 <niclastimle@icloud.com>
2023-06-01 23:19:06 +0200

    Page scrolling still borked

--- src/app/pagecomponents/startpage/startpage.component.ts
+++ src/app/pagecomponents/startpage/startpage.component.ts
@@ -1,5 +1,4 @@
 import { ViewEncapsulation, Component, ElementRef, ViewChild } from '@angular/core';
-import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'startpage-component',
@@ -9,6 +8,7 @@ import { elementAt } from 'rxjs';
 })
 export class StartPageComponent {
 
+  scrollCompleted : boolean = true;
   private readonly sectionOneInput: string = "sectionOneInput";
   private readonly sectionTwoInput: string = "sectionTwoInput";
   private readonly sectionThreeInput: string = "sectionThreeInput";
@@ -35,59 +35,49 @@ export class StartPageComponent {
 
   onScroll(event: WheelEvent) {
     const deltaY = event.deltaY;
-    if (deltaY > 0) this.scrollDown();
-    else if(deltaY < 0) this.scrollUp();
+    if (deltaY > 50) this.scrollDown();
+    else if(deltaY < -90) this.scrollUp();
   }
 
-  private scrollDown() {
+  private async scrollDown() {
     if (this.sectionOne.nativeElement.getBoundingClientRect().bottom > 0) {
-      this.HandleScrollingEvent(this.sectionTwo,this.sectionOneInput,this.sectionTwoInput);
+      await this.HandleScrollingEvent(this.sectionTwo,this.sectionOneInput,this.sectionTwoInput);
     } 
     else if (this.sectionTwo.nativeElement.getBoundingClientRect().bottom > 0) {
-      this.HandleScrollingEvent(this.sectionThree,this.sectionTwoInput,this.sectionThreeInput);
+      await this.HandleScrollingEvent(this.sectionThree,this.sectionTwoInput,this.sectionThreeInput);
     }
     else if (this.sectionThree.nativeElement.getBoundingClientRect().bottom > 0) {
-      this.HandleScrollingEvent(this.sectionFour,this.sectionThreeInput,this.sectionFourInput);
-    }/*TODO because section is mutch smaller then the other, our logic thinks that we are scrolling from section 4.
-            We need to solve the code behinde somehow.
+      await this.HandleScrollingEvent(this.sectionFour,this.sectionThreeInput,this.sectionFourInput);
+    }
     else if (this.sectionFour.nativeElement.getBoundingClientRect().bottom > 0) {
-      this.HandleScrollingEvent(this.sectionFive,this.sectionFourInput,this.sectionFiveInput);
-    }*/
-  }
-
-  private scrollUp() {
-    /* TODO because section is mutch smaller then the other, our logic thinks that we are scrolling from section 4.
-            We need to solve the code behinde somehow.
-    if (this.sectionFive.nativeElement.getBoundingClientRect().top < window.innerHeight) {
-      this.HandleScrollingEvent(this.sectionFour,this.sectionFiveInput,this.sectionFourInput);
-    }*/
-    if (this.sectionFour.nativeElement.getBoundingClientRect().top < window.innerHeight) {
-      this.HandleScrollingEvent(this.sectionThree,this.sectionFourInput,this.sectionThreeInput);
-    }
-    else if (this.sectionThree.nativeElement.getBoundingClientRect().top < window.innerHeight) {
-      this.HandleScrollingEvent(this.sectionTwo,this.sectionThreeInput,this.sectionTwoInput);
-    }
-    else if (this.sectionTwo.nativeElement.getBoundingClientRect().top < window.innerHeight) {
-      this.HandleScrollingEvent(this.sectionOne,this.sectionTwoInput,this.sectionOneInput);
+      await this.HandleScrollingEvent(this.sectionFive,this.sectionFourInput,this.sectionFiveInput);
     }
   }
 
-  private HandleScrollingEvent(
+  private async scrollUp() {
+      if (this.sectionFive.nativeElement.getBoundingClientRect().top < window.innerHeight) {
+        await this.HandleScrollingEvent(this.sectionFour,this.sectionFiveInput,this.sectionFourInput);
+      }
+      else if (this.sectionFour.nativeElement.getBoundingClientRect().top < window.innerHeight) {
+        await this.HandleScrollingEvent(this.sectionThree,this.sectionFourInput,this.sectionThreeInput);
+      }
+      else if (this.sectionThree.nativeElement.getBoundingClientRect().top < window.innerHeight) {
+       await this.HandleScrollingEvent(this.sectionTwo,this.sectionThreeInput,this.sectionTwoInput);
+      }
+      else if (this.sectionTwo.nativeElement.getBoundingClientRect().top < window.innerHeight) {
+        await this.HandleScrollingEvent(this.sectionOne,this.sectionTwoInput,this.sectionOneInput);
+      }
+  }
+
+  private async HandleScrollingEvent(
     scrollToElement: ElementRef,
     elementIdOne: string,
     elementIdTwo: string)
     {
-      setTimeout(() => 
-      {
-        this.scrollTo(scrollToElement);
-        this.setInputToChecked(
-        elementIdOne,
-        elementIdTwo);
-      },300);
-  }
-  scrollTo(element: ElementRef) {
-      element.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  }
+      scrollToElement.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      this.setInputToChecked(elementIdOne, elementIdTwo);
+    }
+
   setInputToChecked(elementIdOne: string, elementIdTwo: string) {
     var element = <HTMLInputElement>document.getElementById(elementIdOne);
     element.checked = false;

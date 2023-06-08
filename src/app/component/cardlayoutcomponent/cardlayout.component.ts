d193c3ee83c27288f7adc03fcf9f7307f6e38490
Carpenteri1 <niclastimle@icloud.com>
2023-06-08 21:46:08 +0200

    Add repos to card view

--- src/app/component/cardlayoutcomponent/cardlayout.component.ts
+++ src/app/component/cardlayoutcomponent/cardlayout.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { trigger, style, transition, animate } from '@angular/animations';
 
 @Component({
     selector: 'cardlayout-component',
@@ -18,16 +17,25 @@ export class CardLayoutComponent implements OnInit {
 
     constructor() {
       this.cardGroupOne.push(
-        new Card('Card title 1 Group 1', 'image1.jpg', 'Lorem ipsum 1', 'Last updated 3 mins ago'),
-        new Card('Card title 2 Group 1', 'image2.jpg', 'Lorem ipsum 2', 'Last updated 5 mins ago'),
-        new Card('Card title 3 Group 1', 'image3.jpg', 'Lorem ipsum 3', 'Last updated 10 mins ago'),
-        new Card('Card title 4 Group 1', 'image4.jpg', 'Lorem ipsum 4', 'Last updated 15 mins ago')
+        new Card('Notify', 'https://cdn.pixabay.com/photo/2016/02/07/14/45/smartphone-1184883_1280.png', 
+        'Android App, Helps to Keep Notes Of Things. In alpha stage at the moment. There is an apk file to try it out,'
+        ,"https://github.com/Carpenteri1/Notify"),
+        new Card('SWAPI-Web-Scrapper', 'image2.jpg', 
+        'Collect starwars characters from swapi, user can search for characters and add them to a list.'
+        ,"https://github.com/Carpenteri1/SWAPI-Web-Scrapper"),
+        new Card('WeatherApp', 'image3.jpg', 
+        'Collects weather data using openweather api,' + 'There is a console verson and an mvc version.'
+        ,"https://github.com/Carpenteri1/WeatherApp"),
       );
       this.cardGroupTwo.push(
-        new Card('Card title 1 Group 2', 'image1.jpg', 'Lorem ipsum 1', 'Last updated 3 mins ago'),
-        new Card('Card title 2 Group 2', 'image2.jpg', 'Lorem ipsum 2', 'Last updated 5 mins ago'),
-        new Card('Card title 3 Group 2', 'image3.jpg', 'Lorem ipsum 3', 'Last updated 10 mins ago'),
-        new Card('Card title 4 Group 2', 'image4.jpg', 'Lorem ipsum 4', 'Last updated 15 mins ago')
+        new Card('Portfolio', 'image1.jpg', 
+        'Website writen with Anguler with typescript. Wanted to try something new when I wrote my portfolio.'
+        ,"https://github.com/Carpenteri1/Portfolio"),
+        new Card('Falling-Rocks', 'image2.jpg', 
+        'a windows console game. The player should avoide the rocks falling from the sky, with its little ship. '
+        ,"https://github.com/Carpenteri1/Falling-Rocks"),
+        new Card('KrankenHause', 'image3.jpg', 'here are 4 different threads who should work together. simulating a hospital'
+        ,"https://github.com/Carpenteri1/KrankenHause"),
       );
     }
 
@@ -50,12 +58,12 @@ export class Card {
     title: string;
     image: string;
     text: string;
-    lastUpdated: string;
+    url: string;
   
-    constructor(title: string, image: string, text: string, lastUpdated: string) {
+    constructor(title: string, image: string, text: string, url: string) {
       this.title = title;
       this.image = image;
       this.text = text;
-      this.lastUpdated = lastUpdated;
+      this.url = url;
     }
   }

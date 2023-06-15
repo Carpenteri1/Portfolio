44f3991b000d715e1ef34ed0a56fae847f8b08dd
Carpenteri1 <niclastimle@icloud.com>
2023-06-15 21:12:50 +0200

    changed color, structure code, layout

--- src/app/component/cardlayoutcomponent/cardlayout.component.ts
+++ src/app/component/cardlayoutcomponent/cardlayout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { CardModel } from "src/app/Models/CardModel";
 
 @Component({
     selector: 'cardlayout-component',
@@ -8,33 +9,33 @@ import { Component, OnInit } from "@angular/core";
 
 export class CardLayoutComponent implements OnInit {
     currentCardIndex: number = 0;
-    cardGroupOne: Card[] = [];
-    cardGroupTwo: Card[] = [];
+    cardGroupOne: CardModel[] = [];
+    cardGroupTwo: CardModel[] = [];
 
     ngOnInit() {
     }    
-    //TODO in memory data, removem when offical data is added
 
     constructor() {
       this.cardGroupOne.push(
-        new Card('Notify', 'https://cdn.pixabay.com/photo/2016/02/07/14/45/smartphone-1184883_1280.png', 
+        new CardModel('Notify', 
         'Android App, Helps to Keep Notes Of Things. In alpha stage at the moment. There is an apk file to try it out,'
         ,"https://github.com/Carpenteri1/Notify"),
-        new Card('SWAPI-Web-Scrapper', 'image2.jpg', 
+        new CardModel('SWAPI-API', 
         'Collect starwars characters from swapi, user can search for characters and add them to a list.'
         ,"https://github.com/Carpenteri1/SWAPI-Web-Scrapper"),
-        new Card('WeatherApp', 'image3.jpg', 
+        new CardModel('WeatherApp', 
         'Collects weather data using openweather api,' + 'There is a console verson and an mvc version.'
         ,"https://github.com/Carpenteri1/WeatherApp"),
       );
       this.cardGroupTwo.push(
-        new Card('Portfolio', 'image1.jpg', 
+        new CardModel('Portfolio', 
         'Website writen with Anguler with typescript. Wanted to try something new when I wrote my portfolio.'
         ,"https://github.com/Carpenteri1/Portfolio"),
-        new Card('Falling-Rocks', 'image2.jpg', 
+        new CardModel('Falling-Rocks', 
         'a windows console game. The player should avoide the rocks falling from the sky, with its little ship. '
         ,"https://github.com/Carpenteri1/Falling-Rocks"),
-        new Card('KrankenHause', 'image3.jpg', 'here are 4 different threads who should work together. simulating a hospital'
+        new CardModel('KrankenHause', 
+         'here are 4 different threads who should work together. simulating a hospital'
         ,"https://github.com/Carpenteri1/KrankenHause"),
       );
     }
@@ -53,17 +54,3 @@ export class CardLayoutComponent implements OnInit {
           }
       }     
 }
-//TODO maybe move to model fo
-export class Card {
-    title: string;
-    image: string;
-    text: string;
-    url: string;
-  
-    constructor(title: string, image: string, text: string, url: string) {
-      this.title = title;
-      this.image = image;
-      this.text = text;
-      this.url = url;
-    }
-  }

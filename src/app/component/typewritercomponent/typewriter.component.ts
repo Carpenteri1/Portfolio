5b5d4792be1744ef0242a3b2ebd52273c58caee3
Niclas Timle <niclastimle@MacBook-Pro-som-tillhor-Niclas.local>
2023-09-03 14:08:49 +0200

    blinking cursor on typewriter

--- src/app/component/typewritercomponent/typewriter.component.ts
+++ src/app/component/typewritercomponent/typewriter.component.ts
@@ -10,6 +10,12 @@ export class TypewriterComponent implements OnInit {
   titleOne = StringHandler.stringEmpy;
   titleTwo = StringHandler.stringEmpy;
   textValue = StringHandler.stringEmpy;
+  //#region Cursor types
+  titleOneCursor = StringHandler.stringEmpy;
+  titleTwoCursor = StringHandler.stringEmpy;
+  cursor = StringHandler.stringEmpy;
+  blinkingCursor = StringHandler.stringEmpy;
+  //#endregion
   private readonly delayTitles = 180;
   private readonly delayText = 60;
 
@@ -21,10 +27,15 @@ export class TypewriterComponent implements OnInit {
   {
     this.titleOne += titleValue[fromIndex];
     fromIndex++;
-
+    this.titleOneCursor = StringHandler.TypeCursor;
     if (fromIndex < titleValue.length)
       setTimeout(() => this.TypeTitleOne(titleValue,fromIndex), this.delayTitles);
-    else this.TypeTitleTwo(StringHandler.typeWriterTitleTwo,0);
+    else 
+    {
+      this.titleOneCursor = StringHandler.stringEmpy;
+      this.titleTwoCursor = StringHandler.TypeCursor;
+      this.TypeTitleTwo(StringHandler.typeWriterTitleTwo,0);
+    }
   }
 
   private TypeTitleTwo(titleValue : string, fromIndex : number) {
@@ -33,7 +44,10 @@ export class TypewriterComponent implements OnInit {
 
     if (fromIndex < titleValue.length) 
       setTimeout(() => this.TypeTitleTwo(titleValue,fromIndex), this.delayTitles);
-    else this.TypeText(StringHandler.typeWriterText,0);
+    else{
+      this.titleTwoCursor = StringHandler.stringEmpy;
+      this.TypeText(StringHandler.typeWriterText,0);
+    }
   }
 
   private TypeText(text : string, fromIndex : number) 
@@ -41,7 +55,13 @@ export class TypewriterComponent implements OnInit {
     this.textValue += text[fromIndex];
     fromIndex++;
 
-    if (fromIndex < text.length) 
+    if (fromIndex < text.length) {
       setTimeout(() => this.TypeText(text,fromIndex), this.delayText);
+      this.cursor = StringHandler.TypeCursor;
+    }
+    else{
+      this.blinkingCursor = StringHandler.TypeCursor;
+      this.cursor = StringHandler.stringEmpy;
+    }
   }
 }

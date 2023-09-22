730a92e8b83afa910851caece00ebd881a501195
Niclas Timle <niclastimle@MacBook-Pro-som-tillhor-Niclas.local>
2023-09-22 23:05:55 +0200

    Css transformation on cards

--- src/app/component/cardlayoutcomponent/cardlayout.component.ts
+++ src/app/component/cardlayoutcomponent/cardlayout.component.ts
@@ -3,55 +3,43 @@ import { CardModel } from "src/app/Models/CardModel";
 import { StringHandler } from "src/app/Utility/stringhandler";
 
 @Component({
-    selector: 'cardlayout-component',
-    templateUrl: 'cardlayout.component.html',
-    styleUrls: ['cardlayout.component.css'],
+  selector: 'cardlayout-component',
+  templateUrl: 'cardlayout.component.html',
+  styleUrls: ['cardlayout.component.css'],
 })
 
-export class CardLayoutComponent implements OnInit {
-    currentCardIndex: number = 0;
-    cardGroupOne: CardModel[] = [];
-    cardGroupTwo: CardModel[] = [];
+export class CardLayoutComponent {
+  cardGroupOne: CardModel[] = [];
+  cardGroupTwo: CardModel[] = [];
 
-    ngOnInit() {
-    }    
+  slideIn = true;
 
+  toggleSlides() {
+    this.slideIn = !this.slideIn;
+  }
+  
   constructor() {
-      this.cardGroupOne.push(
-        new CardModel(StringHandler.cardTitleOne, 
+    this.cardGroupOne.push(
+      new CardModel(StringHandler.cardTitleOne,
         StringHandler.cardDescriptionOne,
         StringHandler.CardLinkOne),
-        new CardModel(StringHandler.cardTitleTwo, 
+      new CardModel(StringHandler.cardTitleTwo,
         StringHandler.cardDescriptionTwo,
         StringHandler.CardLinkTwo),
-        new CardModel(StringHandler.cardTitleThree, 
+      new CardModel(StringHandler.cardTitleThree,
         StringHandler.cardDescriptionThree,
         StringHandler.CardLinkThree),
-      );
-      this.cardGroupTwo.push(
-        new CardModel(StringHandler.cardTitleFour, 
+    );
+    this.cardGroupTwo.push(
+      new CardModel(StringHandler.cardTitleFour,
         StringHandler.cardDescriptionFour,
         StringHandler.CardLinkFour),
-        new CardModel(StringHandler.cardTitleFive, 
-        StringHandler.CardLinkFive,
+      new CardModel(StringHandler.cardTitleFive,
+        StringHandler.cardDescriptionFive,
         StringHandler.CardLinkFive),
-        new CardModel(StringHandler.cardTitleSix, 
-         StringHandler.cardDescriptionSix,
-         StringHandler.CardLinkSix),
-      );
-    }
-
-    navigate(direction: string) {  
-        if (direction === StringHandler.left) {
-            if(this.currentCardIndex === 0)
-                this.currentCardIndex = (this.currentCardIndex + 1);
-            else this.currentCardIndex = (this.currentCardIndex - 1);
-          } 
-          else if (direction === StringHandler.right) 
-          {
-            if(this.currentCardIndex === 1)
-                this.currentCardIndex = (this.currentCardIndex - 1);
-            else this.currentCardIndex = (this.currentCardIndex + 1);
-          }
-      }     
+      new CardModel(StringHandler.cardTitleSix,
+        StringHandler.cardDescriptionSix,
+        StringHandler.CardLinkSix),
+    );
+  }
 }

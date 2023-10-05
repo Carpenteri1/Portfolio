b2e16e7a92b9f84775191e8cc332e0b222dcab4e
Carpenteri1 <niclastimle@icloud.com>
2023-10-05 23:02:08 +0200

    removed import for animation

--- src/app/component/cardlayoutcomponent/cardlayout.component.ts
+++ src/app/component/cardlayoutcomponent/cardlayout.component.ts
@@ -1,7 +1,6 @@
 import { Component } from "@angular/core";
 import { CardModel } from "src/app/Models/CardModel";
 import { StringHandler } from "src/app/Utility/stringhandler";
-import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'cardlayout-component',

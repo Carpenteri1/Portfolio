9a14b57bca980eba63b155b4da07422cc9ba16c7
Carpenteri1 <niclastimle@icloud.com>
2023-10-05 23:01:25 +0200

    now change group of cards when group is not displayed

--- src/app/component/cardlayoutcomponent/cardlayout.component.ts
+++ src/app/component/cardlayoutcomponent/cardlayout.component.ts
@@ -9,10 +9,10 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   styleUrls: ['cardlayout.component.css'],
 })
 
-export class CardLayoutComponent{
+export class CardLayoutComponent {
 
   cardGroup: CardModel[] = [];
-  startIndex: number = 0; 
+  startIndex: number = 0;
   cardWidth = 0;
   /*TODO 
   should be able to do something like [style]="'display(displayType)'" on class="card-group" in html. We can then hide the cards.
@@ -20,8 +20,7 @@ export class CardLayoutComponent{
   so it looks like they swipe in from the right.
   */
   displayType = "none";
-  getVisibleCards(): any[] 
-  {
+  getVisibleCards(): any[] {
     const startIndex = this.startIndex;
     const endIndex = startIndex + 3;
     return this.cardGroup.slice(startIndex, endIndex);
@@ -29,24 +28,21 @@ export class CardLayoutComponent{
 
   toggleGroup(direction: 'left' | 'right'): void {
     const cardCount = this.getVisibleCards().length;
-
     if (direction === 'left') {
-      this.startIndex = (this.startIndex - 1 + cardCount) % cardCount;
       this.cardWidth = -2000;
-      setTimeout(() =>{
-        this.startIndex = (this.startIndex + 1) % cardCount;
-        this.cardWidth = 0;
-      },2000)
-    } else if (direction === 'right') {
-      this.startIndex = (this.startIndex + 1) % cardCount;
-      this.cardWidth = 2000;
-      setTimeout(() =>{
+      setTimeout(() => {
         this.startIndex = (this.startIndex - 1 + cardCount) % cardCount;
         this.cardWidth = 0;
-      },2000)
+      }, 2000)
+    } else if (direction === 'right') {
+      this.cardWidth = 2000;
+      setTimeout(() => {
+        this.startIndex = (this.startIndex + 1) % cardCount;
+        this.cardWidth = 0;
+      }, 2000)
     }
   }
-  
+
   constructor() {
     this.cardGroup.push(
       new CardModel(StringHandler.cardTitleOne,
@@ -58,15 +54,15 @@ export class CardLayoutComponent{
       new CardModel(StringHandler.cardTitleThree,
         StringHandler.cardDescriptionThree,
         StringHandler.CardLinkThree),
-        new CardModel(StringHandler.cardTitleFour,
-          StringHandler.cardDescriptionFour,
-          StringHandler.CardLinkFour),
-        new CardModel(StringHandler.cardTitleFive,
-          StringHandler.cardDescriptionFive,
-          StringHandler.CardLinkFive),
-        new CardModel(StringHandler.cardTitleSix,
-          StringHandler.cardDescriptionSix,
-          StringHandler.CardLinkSix),
+      new CardModel(StringHandler.cardTitleFour,
+        StringHandler.cardDescriptionFour,
+        StringHandler.CardLinkFour),
+      new CardModel(StringHandler.cardTitleFive,
+        StringHandler.cardDescriptionFive,
+        StringHandler.CardLinkFive),
+      new CardModel(StringHandler.cardTitleSix,
+        StringHandler.cardDescriptionSix,
+        StringHandler.CardLinkSix),
     );
   }
 }

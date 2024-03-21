2d8eb98328300e3864379edc5cc76f4371d07222
Carpenteri1 <niclastimle@icloud.com>
2024-03-21 00:58:07 +0000

    Deploying to gh-pages from @ Carpenteri1/Portfolio@f03c66cf04cb68c21add8dcd07fa7c6a226614a9 🚀

--- src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { StringHandler } from './Utility/stringhandler';
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
-})
-export class AppComponent {
-  title = StringHandler.pageTitle;
-}

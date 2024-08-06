78077f1c5e9af75617df3366c13016055af47bd1
Niclas Timle <niclastimle@Niclass-MacBook-Air.local>
2024-08-06 21:36:21 +0200

    fix scrolling for phone and mouse

--- src/app/component/scrollbuttoncomponent/maincompontent/scrollbutton.component.ts
+++ src/app/component/scrollbuttoncomponent/maincompontent/scrollbutton.component.ts
@@ -10,12 +10,13 @@ import { ViewEncapsulation, Component, ElementRef, Input, HostListener } from '@
 export class ScrollButtonComponent {
   
   @Input() sections!: { [key: string]: ElementRef };
+  private startTouchY: number = 0;
+  private scrollTimeout: any = null;
   currentSection: number = 1;
 
   @HostListener('window:keydown', ['$event'])
   HandleKeyboardEvent(event: KeyboardEvent) {
     const key = event.key.toLowerCase();
-
     if (key === 'w' || key === 'arrowup') 
         this.ScrollToSection(this.currentSection - 1);
     if (key === 's' || key === 'arrowdown') 
@@ -25,24 +26,34 @@ export class ScrollButtonComponent {
   OnClick(section: ElementRef){
     this.ScrollToSection(parseInt(section.nativeElement.id))
   }
-/*
-  OnMouseScroll(event: WheelEvent) {
-    const deltaY = event.deltaY;
-    if(deltaY > 50) this.ScrollToSection(this.currentSection - 1);
-    if(deltaY < -90) this.ScrollToSection(this.currentSection + 1);
+
+  @HostListener('window:touchstart', ['$event'])
+  onTouchStart(event: TouchEvent): void {
+    if (event.touches.length > 0) {
+      this.startTouchY = event.touches[0].clientY;
+    }
   }
 
-  OnTouchScroll(event: TouchEvent) {
-    const deltaY = event;
-    if(deltaY > 50) this.ScrollToSection(this.currentSection - 1);
-    if(deltaY < -90) this.ScrollToSection(this.currentSection + 1);
+  @HostListener('window:touchend', ['$event'])
+  onTouchEnd(event: TouchEvent): void {
+    if (event.changedTouches.length > 0) {
+      const deltaY = this.startTouchY - event.changedTouches[0].clientY;
+      this.ClearTimeOut();
+      this.SetTimeOutForScroll(25, this.startTouchY - event.changedTouches[0].clientY);
+    }
   }
-  */
+
+  @HostListener('window:wheel', ['$event'])
+  onWheelEvent(event: WheelEvent): void {
+    event.deltaY;
+    this.ClearTimeOut();
+    this.SetTimeOutForScroll(40,event.deltaY);
+  }
   
   ScrollToSection(scrollTo: number) {
     if (scrollTo >= 1 && scrollTo <= 4) {
       this.sections[scrollTo].nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-      this.SetInputToChecked(this.sections[this.currentSection].nativeElement,this.sections[scrollTo].nativeElement) //TODO maybe remove note needed
+      this.SetInputToChecked(this.sections[this.currentSection].nativeElement,this.sections[scrollTo].nativeElement)
       this.currentSection = scrollTo;
     }
     if(this.currentSection < 1)
@@ -51,16 +62,24 @@ export class ScrollButtonComponent {
       this.currentSection = 4    
   }
 
-  /*
-  onScroll(event: WheelEvent) {
-    const deltaY = event.deltaY;
-    if (deltaY > 50) this.scrollDown();
-    else if(deltaY < -90) this.scrollUp();
-  }
-  */
-
   SetInputToChecked(elementIdOne: any, elementIdTwo: any) {
     elementIdOne.checked = false;
     elementIdTwo.checked = true;
+  }
+
+  private ClearTimeOut(){
+    if (this.scrollTimeout) {
+      clearTimeout(this.scrollTimeout);
+    }
+  }
+
+  private SetTimeOutForScroll(timeOut: number, scrollDirection: number){
+    this.scrollTimeout = setTimeout(() => {
+      if (scrollDirection > 0) {
+        this.ScrollToSection(this.currentSection + 1);
+      } else {
+        this.ScrollToSection(this.currentSection - 1);
+      }
+    },timeOut);
   }
 }

4c1a40d0efd7fd003f608be015b00eecb3dda255
Niclas Timle <niclastimle@Niclass-MacBook-Air.local>
2024-08-28 21:30:12 +0200

    added icon, changed string data, layout removed css and more

--- src/app/Utility/stringhandler.ts
+++ src/app/Utility/stringhandler.ts
@@ -24,20 +24,20 @@ export class StringHandler {
     //#region Cardlayout
 
     //Titles
-    static readonly cardTitleOne: string = 'Notify';
-    static readonly cardTitleTwo: string = 'SWAPI-API';
-    static readonly cardTitleThree: string = 'WeatherApp';
-    static readonly cardTitleFour: string = 'Portfolio';
-    static readonly cardTitleFive: string = 'Falling-Rocks';
-    static readonly cardTitleSix: string = 'KrankenHause';
+    static readonly CardTitleNotify: string = 'Notify';
+    static readonly CardTitleSwapi: string = 'SWAPI-API';
+    static readonly CardTitleWeatherApp: string = 'WeatherApp';
+    static readonly CardTitlePortfolio: string = 'Portfolio';
+    static readonly CardTitleFallenRock: string = 'Falling-Rocks';
+    static readonly CardTitleKrankenHouse: string = 'KrankenHause';
    
     //Description
-    static readonly cardDescriptionOne: string = 'Android App, Helps to Keep Notes Of Things. In alpha stage at the moment';
-    static readonly cardDescriptionTwo: string = 'Search for starwars characters and add them to a list.';
-    static readonly cardDescriptionThree: string = 'Show weather data using openweather, There is a console app and web app';
-    static readonly cardDescriptionFour: string = 'My porfolio, a website with a short description on my projects and skills';
-    static readonly cardDescriptionFive: string = 'Terminal game. The player should avoide the rocks falling from the sky';
-    static readonly cardDescriptionSix: string = 'Using 4 different threads who should work together. simulating different roles';
+    static readonly CardDescriptionNotify: string = 'Android App, Helps to Keep Notes Of Things. In alpha stage at the moment';
+    static readonly CardDescriptionSwapi: string = 'Search for starwars characters and add them to a list.';
+    static readonly CardDescriptionWeatherApp: string = 'Show weather data using openweather, There is a console app and web app';
+    static readonly CardDescriptionPortolio: string = 'My porfolio, a website with a short description on my projects and skills';
+    static readonly CardDescriptionFallenRock: string = 'Terminal game. The player should avoide the rocks falling from the sky';
+    static readonly CardDescriptionKrankenHause: string = 'Using 4 different threads who should work together. simulating different roles';
     
     //Tags
     static readonly CardTagAngular: string = "Angular";
@@ -53,16 +53,21 @@ export class StringHandler {
     static readonly CardTagNPM: string = "NPM";
     static readonly CardTagXML: string = "XML";
 
-
-
+    //Icons
+    static readonly AndroidIcon: string = "bi bi-android2";
+    static readonly CloudIcon: string = "bi bi-cloud-sun-fill";
+    static readonly StarsIcon: string = "bi bi-stars";
+    static readonly BriefCaseIcon: string = "bi bi-briefcase-fill";
+    static readonly DpadIcon: string = "bi bi-dpad-fill";
+    static readonly CpuIcon: string = "bi bi-cpu-fill";
 
     //Links
-    static readonly CardLinkOne: string = "https://github.com/Carpenteri1/Notify";
-    static readonly CardLinkTwo: string = "https://github.com/Carpenteri1/SWAPI-Web-Scrapper";
-    static readonly CardLinkThree: string = "https://github.com/Carpenteri1/WeatherApp";
-    static readonly CardLinkFour: string = "https://github.com/Carpenteri1/Portfolio";
-    static readonly CardLinkFive: string = "https://github.com/Carpenteri1/Falling-Rocks";
-    static readonly CardLinkSix: string = "https://github.com/Carpenteri1/KrankenHause";
+    static readonly CardLinkNotify: string = "https://github.com/Carpenteri1/Notify";
+    static readonly CardLinkSwapi: string = "https://github.com/Carpenteri1/SWAPI-Web-Scrapper";
+    static readonly CardLinkWeatherApp: string = "https://github.com/Carpenteri1/WeatherApp";
+    static readonly CardLinkPortfolio: string = "https://github.com/Carpenteri1/Portfolio";
+    static readonly CardLinkFallenRock: string = "https://github.com/Carpenteri1/Falling-Rocks";
+    static readonly CardLinkKrankenHause: string = "https://github.com/Carpenteri1/KrankenHause";
 
     //#endregion
 

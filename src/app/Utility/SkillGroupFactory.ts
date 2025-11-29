5f8fb645070f7e8d287ed4bbb9ee459f415f945d
niclastimle <niclastimle@icloud.com>
2025-11-29 12:11:02 +0100

    fixed fallback for Stacks

--- src/app/Utility/SkillGroupFactory.ts
+++ src/app/Utility/SkillGroupFactory.ts
@@ -2,13 +2,13 @@ import {SkillGroupModel} from "../Models/SkillGroupModel";
 export function MapSkillGroupData(model: SkillGroupModel) : SkillGroupModel{
   return {
     Title: model?.Title ?? "",
-    Stacks: model?.Stacks ?? undefined,
+    Stacks: model?.Stacks ?? [],
   } as SkillGroupModel;
 }
 
 MapSkillGroupData.Override = function(override: Partial<SkillGroupModel>, model?: SkillGroupModel): SkillGroupModel {
   return MapSkillGroupData({
     Title: override.Title ?? model?.Title ?? "",
-    Stacks: override.Stacks ??  model?.Stacks ?? 0,
+    Stacks: override.Stacks ??  model?.Stacks ?? [],
   } as SkillGroupModel);
 };
